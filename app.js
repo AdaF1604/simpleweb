7d344f0ef0e369c987ac3bcc934d95006373f641
Mi Compu <ada.finamore.1@gmail.com>
2022-01-04 17:38:28 -0300

    funciones boton

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function () {
+	document.body.ClassList.toggle('dark-theme');
+
+	var className = document.body.className;
+	if(className == "light-theme") {
+		this.textContent = "Dark";
+
+	}
+	else {
+		this.textContent = "Light";
+
+	}
+
+});
